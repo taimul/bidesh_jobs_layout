f383f50b6c86b19d423c0bd023c1ace609d887f3
taimul <taimul.haque@gmail.com>
2023-04-11 04:34:32 +0600

    banner section for desktop added

---
+++ src/Components/Home/Hero/Hero.js
@@ -0,0 +1,258 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { GoBriefcase, GoGlobe, GoLocation } from "react-icons/go";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { TbTool } from "react-icons/tb";
+import image0 from "../../../Assets/slide-00.jpg";
+import image1 from "../../../Assets/slide-01.jpg";
+import image2 from "../../../Assets/slide-02.jpg";
+import image3 from "../../../Assets/slide-03.jpg";
+import image4 from "../../../Assets/slide-04.jpg";
+import image5 from "../../../Assets/slide-05.jpg";
+import bg1 from "../../../Assets/banner-bg-l.png";
+import bg2 from "../../../Assets/banner-bg-r.png";
+import submitBio from "../../../Assets/submit_biodata.png";
+
+const Hero = () => {
+  return (
+    <div className="">
+      <div className="">
+        <div className="flex flex-wrap">
+          <div
+            className="w-full sm:w-1/2 md:w-1/3  h-[450px] bg-repeat-x bg-bottom"
+            style={{
+              backgroundImage: `url(${bg1})`,
+            }}
+          >
+            <div className="">
+              <div className="text-white bg-[#2E7FDA] bg-opacity-80  h-[450px]">
+                <div className="flex flex-row justify-end">
+                  <div className="p-10">
+                    <div>
+                      <h3 className="mb-4 font-bold text-xl">
+                        সঠিক চাকরি খুঁজে নিন
+                      </h3>
+                      <h4 className="mb-4 font-bold flex">
+                        <span className="text-xl mr-2">
+                          <GoBriefcase />
+                        </span>
+                        কাঙ্ক্ষিত পেশা
+                      </h4>
+                      <input
+                        type="text"
+                        placeholder="ড্রাইভার, ইলেকট্রিশিয়ান ইত্যাদি"
+                        className="input w-[262px] text-xs bg-white mb-4 text-gray-500"
+                      />
+                    </div>
+                    <div>
+                      <h4 className="mb-4 font-bold flex">
+                        <span className="text-xl mr-2">
+                          <GoGlobe />
+                        </span>
+                        দক্ষতার ধরন
+                      </h4>
+                      <select className="select w-[262px] bg-white text-gray-500">
+                        <option disabled selected>
+                          দক্ষতার ধরন নির্বাচন
+                        </option>
+                        <option>সম্পূর্ণ দক্ষ</option>
+                        <option>মাঝারি দক্ষ</option>
+                        <option>কম দক্ষ</option>
+                      </select>
+                    </div>
+                    <div>
+                      <h4 className="mb-4 font-bold flex mt-4">
+                        <span className="text-xl mr-2">
+                          <GoLocation />
+                        </span>
+                        দেশ
+                      </h4>
+                      <select className="select w-[262px] bg-white text-gray-500">
+                        <option disabled selected>
+                          দেশ নির্বাচন
+                        </option>
+                        <option>
+                          সংযুক্ত আরব আমীরাত (United Arab Emirates)
+                        </option>
+                        <option>সৌদি আরব (Saudi Arabia)</option>
+                        <option>জাপান (Japan)</option>
+                        <option>দক্ষিণকোরিয়া (South Korea)</option>
+                        <option>মালয়েশিয়া (Malaysia)</option>
+                        <option>ওমান (Oman)</option>
+                        <option>জর্ডান (Jordan)</option>
+                        <option>ডেনমার্ক (Denmark)</option>
+                      </select>
+                    </div>
+                    <button className="btn btn-warning mt-4">অনুসন্ধান</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/3 h-[450px] bg-white">
+            <div className="">
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 3500,
+                  disableOnInteraction: false,
+                }}
+                navigation={true}
+                modules={[Autoplay, Navigation]}
+                className="mySwiper "
+              >
+                <SwiperSlide>
+                  <img src={image0} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image3} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image3} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image4} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={image5} alt="" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+          <div
+            className="w-full sm:w-1/2 md:w-1/3  h-[450px] bg-repeat-x bg-bottom"
+            style={{
+              backgroundImage: `url(${bg2})`,
+            }}
+          >
+            <div className="">
+              <div className="text-white bg-[#2E7FDA] bg-opacity-80  h-[470px]">
+                <div className="flex flex-row justify-start">
+                  <div className="p-4">
+                    <div>
+                      <h3 className="mb-3 font-bold mt-4">
+                        বিভিন্ন দেশে কর্মী প্রয়োজন
+                      </h3>
+                      <ul>
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbna71a.html?locationId=310">
+                            সংযুক্ত আরব আমীরাত (৪৫)
+                          </a>
+                        </li>
+
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbn470b.html?locationId=276">
+                            সৌদি আরব (৩৫)
+                          </a>
+                        </li>
+
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbnafa2.html?locationId=202">
+                            জাপান (২৫)
+                          </a>
+                        </li>
+
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbnd62b.html?locationId=287">
+                            দক্ষিণকোরিয়া (১১)
+                          </a>
+                        </li>
+
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbn3f0a.html?locationId=222">
+                            মালয়েশিয়া (৬)
+                          </a>
+                        </li>
+
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbnd71a.html?locationId=252">
+                            ওমান (৬)
+                          </a>
+                        </li>
+                        <li className="mb-3 font-thin text-gray-300">
+                          <a href="jobsearchbnd71a.html?locationId=252">
+                            সব দেশ
+                          </a>
+                        </li>
+                      </ul>
+                      <button className="btn btn-warning bg-[#FFDC79] mt-[6px] font-bold rounded-lg text-gray-950 w-40">
+                        <img src={submitBio} alt="" />
+                        Submit your <br />
+                        Biodata
+                      </button>
+                    </div>
+                    <button className="btn flex flex-col hover:bg-white bg-white mt-[6px] font-bold rounded-lg text-gray-950 w-40">
+                      <img
+                        src="https://bdesh.bdjobs.com/images/bdesh_jatra.svg"
+                        alt=""
+                      />
+                      <div className="text-[10px]">অভিবাসন বিষয়ক তথ্য</div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full  bg-white">
+            <div className="container mx-auto max-w-6xl">
+              <div className="grid grid-cols-3">
+                <div className="flex p-4">
+                  <div className="bg-red-500 rounded-full p-4 inline-block">
+                    <span className="text-white">
+                      {" "}
+                      <HiOutlineUsers className="text-4xl" />
+                    </span>
+                  </div>
+                  <div className="mt-3 ml-4">
+                    <p className="font-bold text-gray-900">কর্মী প্রয়োজন</p>
+                    <p className="text-red-500 font-bold">১৩০</p>
+                  </div>
+                </div>
+                <div className="flex p-4">
+                  <div className="bg-green-500 rounded-full p-4 inline-block">
+                    <span className="text-white">
+                      {" "}
+                      <TbTool className="text-4xl" />
+                    </span>
+                  </div>
+                  <div className="mt-3 ml-4">
+                    <p className="text-gray-900 font-bold">চাকরির সংখ্যা</p>
+                    <p className="font-bold text-green-500">১২</p>
+                  </div>
+                </div>
+                <div className="flex p-4">
+                  <div className="bg-[#FF9800] rounded-full p-4 inline-block">
+                    <span className="text-white">
+                      {" "}
+                      <GoGlobe className="text-4xl" />
+                    </span>
+                  </div>
+                  <div className="mt-3 ml-4">
+                    <p className="text-gray-900 font-bold">
+                      বিভিন্ন দেশের চাকরি
+                    </p>
+                    <p className="text-[#FF9800]">৮</p>
+                  </div>
+                </div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,7 +1,151 @@
 import React from "react";
+import { FaFacebook, FaYoutube, FaUserCircle, FaPhone } from "react-icons/fa";
+import jointVenture from "../../Assets/Joint_venture.png";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="bg-white">
+      <header className="bg-[#ECEFF1] text-gray-900 p-2 text-xs hidden sm:block">
+        <div className="">
+          <div className="container mx-auto max-w-6xl font-bold">
+            <div className="flex justify-between items-center">
+              <div className="flex-1">
+                <ul className="flex space-x-6">
+                  <li>
+                    <a href="https://bdjobs.com/">বিডিজবস্</a>
+                  </li>
+                  <li>
+                    <a href="jobsearchbn.com">চাকরির খবর</a>
+                  </li>
+                  <li>
+                    <a href="https://bdjobs.com/">চাকরিপ্রার্থীর একাউন্ট</a>
+                  </li>
+                  <li>
+                    <a href="https://bdjobs.com/">
+                      নিয়োগকারী সংস্থা/ বিদেশী নিয়োগকর্তা{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex items-center">
+                <ul className="flex space-x-4">
+                  <li>
+                    <button className="bg-blue-500 rounded-full p-1 text-white">
+                      <FaFacebook />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="bg-red-500 rounded-full p-1 text-white">
+                      <FaYoutube />
+                    </button>
+                  </li>
+                </ul>
+                <div className="lang ml-4 border-blue-500 border-2">
+                  <button className="bg-blue-500 p-1 text-white font-thin">
+                    বাংলা
+                  </button>
+                  <button className="p-1 font-thin">ENG</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Second Header start*/}
+      <div className="navbar container mx-auto max-w-6xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li tabIndex={0}>
+                <a href="/" className="justify-between">
+                  Parent
+                  <svg
+                    className="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                  </svg>
+                </a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <button className="hidden sm:block">
+            <img
+              src="https://bdesh.bdjobs.com/images/logo_bdesh.svg"
+              alt=""
+              className="w-40"
+            />
+          </button>
+        </div>
+        <div className="sm:hidden block">
+          <img
+            src="https://bdesh.bdjobs.com/images/logo_bdesh.svg"
+            alt=""
+            className="w-40"
+          />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <img src={jointVenture} alt="" />
+        </div>
+        <div className="navbar-end text-sm">
+          <div className="flex items-center hidden md:inline-block">
+            <ul className="flex items-center">
+              <li className="mr-4 ">
+                <a href="/" className="flex items-center">
+                  <FaUserCircle className="text-blue-700 mr-1 text-lg" />
+                  <span className="hidden md:inline-block text-blue-400">
+                    সাইন ইন / অ্যাকাউন্ট তৈরি
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="/" className="flex items-center text-gray-900">
+                  <FaPhone className=" mr-1" />
+                  <span className="hidden md:inline-block">যোগাযোগ</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
